91c1c1e14df94878684eeb05c203482376d4d40c
LKTProgrammer <gitaek91@naver.com>
2017-07-20 18:52:19 +0900

    Line Inpormation Parsing & Strategy Pattern

---
+++ app/src/main/java/com/example/bsinpormation/Parser_Inter.java
@@ -0,0 +1,17 @@
+package com.example.bsinpormation;
+
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 이기택 on 2017-07-20.
+ */
+
+public interface Parser_Inter {
+
+    public void Parsing_Xml() throws XmlPullParserException;
+    public ArrayList<BusLine_Info> Get_InfoList();
+}
